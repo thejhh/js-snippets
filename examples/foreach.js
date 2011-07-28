c03d1f8e5d0349162c5fb0fc29561f6e437f8eed
Jaakko-Heikki Heusala <jheusala@iki.fi>
2011-07-28 03:18:20 +0300

    Fixed examples

--- examples/foreach.js
+++ examples/foreach.js
@@ -1,26 +1,32 @@
 
 /* Foreach examples */
 
-var foreach = require('snippets').foreach;
+var foreach = require('../lib/snippets.js').foreach;
 
 // Foreach array
 var a = [1,2,3,4,5];
 foreach(a).do(function(v) { console.log(v); });
 
 // Foreach object
-a = {'foo':1,'bar':2];
+a = {'foo':1,'bar':2};
 foreach(a).do(function(v, k) { console.log(k+"="+v); });
 
 // Foreach single variable
 a = 'foobar';
 foreach(a).do(function(v) { console.log(v); });
 
+var foreach = require('../lib/foreach');
+
+// Foreach only arrays
+a = [1,2,3,4,5];
+foreach.any(a).do(function(v) { console.log(v); });
+
 // Foreach only arrays
 a = [1,2,3,4,5];
 foreach.array(a).do(function(v) { console.log(v); });
 
 // Foreach only objects
-a = {'foo':1,'bar':2];
+a = {'foo':1,'bar':2};
 foreach.object(a).do(function(v, k) { console.log(k+"="+v); });
 
 // Foreach only single elements
